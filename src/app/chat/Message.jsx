d59d8764a97006884532bda62719764e1af3ec2e
ashok <ashokzarmariya90@gmail.com>
2024-08-12 11:38:50 +0530

    create crypto project

---
+++ src/app/chat/Message.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Message = () => {
+  return (
+    <div>message Components</div>
+  )
+}
+
+export default Message
---
+++ src/app/chat/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Message from './message'
+
+const page = () => {
+  return (
+    <div>
+        <Message/>
+        <h1>main page</h1>
+    </div>
+  )
+}
+
+export default page
